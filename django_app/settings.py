a3aae93c2aeab9387a414b9a8e211943ad9aaac5
Samuel Hartmann <hartmann.samuel@gmx.de>
2022-05-19 08:13:41 +0200

    setup polls app
    - based on https://docs.djangoproject.com/en/4.0/intro

--- django_app/settings.py
+++ django_app/settings.py
@@ -48,6 +48,7 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split("
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
